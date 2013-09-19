41b5c44cd2468ad7d772578de40e46e4d8cddcf0
Nedzad Kulelija <nedzad@ironhide.(none)>
2013-09-19 19:52:42 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-imgur_screenshot_upload
+Imgur Screenshot Uploader
 =======================
 
 Take screenshot, upload it to imgur and copy link in clipoboard
+
+Requires following python modules pyscreeshot, requests and xclip installed on your system!
+
+You need to get client_id for IMGUR API...do not use mine :) :) :)
+
+
+
+
---
+++ ss.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import pyscreenshot as ImageGrab
+import requests
+import json
+import base64
+import os
+import ConfigParser
+import datetime
+
+
+config = ConfigParser.RawConfigParser()
+config.read('ss.ini')
+img_path = config.get('config', 'img_save_path')
+img_name = config.get('config', 'img_name')
+client_id = config.get('config', 'imgur_client_id')
+
+timestamp = datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+full_path = img_path + img_name + str(timestamp) + '.png'
+
+im = ImageGrab.grab_to_file(full_path)
+url = 'https://api.imgur.com/3/upload.json'
+f = open(full_path, 'rb')
+binary_data = f.read()
+image64 = base64.b64encode(binary_data)
+headers = {'Authorization': 'Client-ID ' + client_id}
+payload = {'key': client_id,
+           'image': image64,
+           'type': 'base64',
+           'title': 'Python ScreenShoter'}
+result = requests.post(url, headers=headers, data=payload)
+jarr = json.loads(result.text)
+link = jarr['data']['link']
+print link
+clip = 'echo ' + link.strip() + '| xclip -selection c'
+os.system(clip)
